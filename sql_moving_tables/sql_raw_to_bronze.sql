a30e88cc99c86ae164c6b39ceb2a5745e322da03
guibusanello <gui_busanello@hotmail.com>
2026-03-17 16:22:52 -0300

    códigos sql para criação dos schemas e movimentação dos dados brutos para o schema bronze no Supabase

---
+++ sql_moving_tables/sql_raw_to_bronze.sql
@@ -0,0 +1,4 @@
+ALTER TABLE crm_accounts SET SCHEMA bronze;
+ALTER TABLE crm_contracts SET SCHEMA bronze;
+ALTER TABLE user_activity_events SET SCHEMA bronze;
+ALTER TABLE support_tickets SET SCHEMA bronze;
